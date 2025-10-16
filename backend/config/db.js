f44e0ca5a32e15cc10f2a5b0d70ec581ba5ed66c
ahmazal <rizalrahmdni47@gmail.com>
2025-10-17 06:19:26 +0700

    update section

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,8 +4,8 @@ const db = mysql.createConnection({
     user: "root",
     password: "",
     database: "",
-    database: "backendLomba",
-    port: 3307
+    database: "ecommerce",
+    port: 3306
 })
 
 db.connect(err => {
